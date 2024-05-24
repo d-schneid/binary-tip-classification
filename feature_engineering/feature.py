eb31190c22cf066668640dd107de5687e65f5c2f
Daniel Schneider <daniel-wernau@t-online.de>
2024-05-24 03:02:39 +0200

    Add static feature mode_dept

--- feature_engineering/feature.py
+++ feature_engineering/feature.py
@@ -3,104 +3,101 @@ from abc import ABC, abstractmethod
 
 class Feature(ABC):
 
-	def __init__(self, data_store, name):
-		self.data_store = data_store
-		self.feature = name
-		self.orders_tip = self._get_orders_tip()
-		self.orders_joined = self._get_orders_joined()
+    def __init__(self, data_store, name):
+        self.data_store = data_store
+        self.feature = name
 
-	@abstractmethod
-	def _get_orders_tip(self):
-		pass
+    def compute_feature(self):
+        self._handle_missing_values()
+        self._compute_feature()
+        if self._reference_outdated():
+            self._update_data_store()
+            self._refresh_references()
 
-	@abstractmethod
-	def _get_orders_joined(self):
-		pass
+    def _handle_missing_values(self):
+        self.orders_tip['days_since_prior_order'] = self.orders_tip['days_since_prior_order'].fillna(-1).astype(int)
+        pass
 
-	def compute_feature(self):
-		self._handle_missing_values()
-		self._compute_feature()
-		if self._reference_outdated():
-			self._update_data_store()
-			self._refresh_references()
+    @abstractmethod
+    def _compute_feature(self):
+        pass
 
-	def _handle_missing_values(self):
-		self.orders_tip['days_since_prior_order'] = self.orders_tip['days_since_prior_order'].fillna(-1).astype(int)
-		pass
+    def analyze_feature(self):
+        pass
 
-	@abstractmethod
-	def _compute_feature(self):
-		pass
+    @abstractmethod
+    def _analyze_feature(self):
+        pass
 
-	def analyze_feature(self):
-		pass
+    @abstractmethod
+    def _refresh_references(self):
+        pass
 
-	@abstractmethod
-	def _analyze_feature(self):
-		pass
+    @abstractmethod
+    def _update_data_store(self):
+        pass
 
-	def _refresh_references(self):
-		self.orders_tip = self._get_orders_tip()
-		self.orders_joined = self._get_orders_joined()
-
-	@abstractmethod
-	def _update_data_store(self):
-		pass
-
-	def _reference_outdated(self):
-		return self.orders_tip is not self._get_orders_tip()
+    @abstractmethod
+    def _reference_outdated(self):
+        pass
 
 
 class StaticFeature(Feature):
 
-	def __init__(self, data_store, name):
-		super().__init__(data_store, name)
+    def __init__(self, data_store, name):
+        super().__init__(data_store, name)
+        self.orders_tip = data_store.get_orders_tip()
+        self.orders_joined = data_store.get_orders_joined()
 
-	def _get_orders_tip(self):
-		return self.data_store.get_orders_tip()
+    def compute_feature(self):
+        self._refresh_references()
+        if self.feature not in self.orders_tip.columns:
+            super().compute_feature()
 
-	def _get_orders_joined(self):
-		return self.data_store.get_orders_joined()
+    @abstractmethod
+    def _compute_feature(self):
+        pass
 
-	def compute_feature(self):
-		self._refresh_references()
-		if self.feature not in self.orders_tip.columns:
-			super().compute_feature()
+    @abstractmethod
+    def _analyze_feature(self):
+        pass
 
-	@abstractmethod
-	def _compute_feature(self):
-		pass
+    def _refresh_references(self):
+        self.orders_tip = self.data_store.get_orders_tip()
+        self.orders_joined = self.data_store.get_orders_joined()
 
-	@abstractmethod
-	def _analyze_feature(self):
-		pass
+    def _update_data_store(self):
+        self.data_store.merge_orders_tip(self.orders_tip, self.feature)
 
-	def _update_data_store(self):
-		self.data_store.merge_orders_tip(self.orders_tip, self.feature)
+    def _reference_outdated(self):
+        return self.orders_tip is not self.data_store.get_orders_tip()
 
 
 class DynamicFeature(Feature):
 
-	def __init__(self, data_store, name):
-		super().__init__(data_store, name)
+    def __init__(self, data_store, name):
+        super().__init__(data_store, name)
+        self.orders_tip = data_store.get_orders_tip_subset()
+        self.orders_joined = data_store.get_orders_joined_subset()
 
-	def _get_orders_tip(self):
-		return self.data_store.get_orders_tip_subset()
+    def compute_feature(self):
+        self._refresh_references()
+        super().compute_feature()
 
-	def _get_orders_joined(self):
-		return self.data_store.get_orders_joined_subset()
+    @abstractmethod
+    def _compute_feature(self):
+        pass
 
-	def compute_feature(self):
-		self._refresh_references()
-		super().compute_feature()
+    @abstractmethod
+    def _analyze_feature(self):
+        pass
 
-	@abstractmethod
-	def _compute_feature(self):
-		pass
+    def _refresh_references(self):
+        self.orders_tip = self.data_store.get_orders_tip_subset()
+        self.orders_joined = self.data_store.get_orders_joined_subset()
 
-	@abstractmethod
-	def _analyze_feature(self):
-		pass
+    def _update_data_store(self):
+        self.data_store.merge_orders_tip_subset(self.orders_tip, self.feature)
 
-	def _update_data_store(self):
-		self.data_store.merge_orders_tip_subset(self.orders_tip, self.feature)
+    def _reference_outdated(self):
+        return self.orders_tip is not self.data_store.get_orders_tip_subset()
--- feature_engineering/features/__init__.py
+++ feature_engineering/features/__init__.py
@@ -3,3 +3,4 @@ from .dynamic_feature_test_2 import DynamicFeatureTest2
 from .order_size import OrderSize
 from .reordered_ratio import ReorderedRatio
 from .tip_history import TipHistory
+from .mode_department import ModeDepartment
