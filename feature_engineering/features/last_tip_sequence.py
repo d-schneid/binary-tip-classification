3470d816f9fa6a9901caff57162f09ef6119fd8b
Max <max.wnstr@gmail.com>
2024-05-24 18:36:53 +0200

    last_tip_sequence / sim_orders_tip_ratio

--- feature_engineering/features/last_tip_sequence.py
+++ feature_engineering/features/last_tip_sequence.py
@@ -7,12 +7,12 @@ class LastTipSequence(StaticFeature):
         super().__init__(data_store, 'last_tip_sequence')
 
     def _compute_feature(self):
-        orders_tip_copy = self.orders_tip.copy()
-        orders_tip_copy['consecutive_tips'] = orders_tip_copy.apply(self._count_last_tips, axis=1)
+        self.orders_tip[self.feature] = self.orders_tip.apply(self._count_last_tips, axis=1)
 
     def _count_last_tips(self, order):
         consecutive_tips = 0
         for i in range(order['order_number'] - 1, 0, -1):
+
             if self.orders_tip[(self.orders_tip.user_id == order.user_id)
                                & (order.order_number == i), 'tip'].values[0] == 1:
                 consecutive_tips += 1
