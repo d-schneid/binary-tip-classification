47feab99649b9abd5f98359b7528d36f7ea04834
I538883 <kilian.kempf@sap.com>
2024-05-24 14:35:00 +0200

    fix previously tipped products ratio

--- feature_engineering/features/prev_tipped_products_ratio.py
+++ feature_engineering/features/prev_tipped_products_ratio.py
@@ -8,11 +8,11 @@ class PrevTippedProductsRatio(StaticFeature):
     def __init__(self, data_store):
         super().__init__(data_store, 'prev_tipped_products_ratio')
 
-    def prev_tipped_products_ratio_calculation(user_orders):
+    def prev_tipped_products_ratio_calculation(self, user_orders):
         cumulative_products = set()
         for idx, order in user_orders.iterrows():
             prev_tipped_products = cumulative_products.intersection(order['products'])
-            user_orders.at[idx, 'prev_tipped_products_ratio'] = len(prev_tipped_products) / len(order['products'])
+            user_orders.at[idx, self.feature] = len(prev_tipped_products) / len(order['products'])
             if order['tip'] == 1.0:
                 cumulative_products.update(order['products'])
         return user_orders
@@ -25,9 +25,7 @@ class PrevTippedProductsRatio(StaticFeature):
                                                    include_groups=False).reset_index(
             drop=False).drop(columns='level_1')
 
-        self.orders_tip = pd.merge(self.orders_tip, grouped[['user_id', 'order_number', 'prev_tipped_products_ratio']],
-                                   on=['user_id', 'order_number'],
-                                   how='left')
+        self.orders_tip = pd.merge(self.orders_tip, grouped[['order_id', self.feature]], on='order_id', how='left')
 
     def _analyze_feature(self):
         pass
