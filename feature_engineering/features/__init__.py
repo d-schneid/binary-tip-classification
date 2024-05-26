062c2cda4b67b643fc42405efd42ab45a2473ec3
I538883 <kilian.kempf@sap.com>
2024-05-26 11:07:31 +0200

    add prev order tipped

--- feature_engineering/features/__init__.py
+++ feature_engineering/features/__init__.py
@@ -1,12 +1,13 @@
 from .customer_lifetime import CustomerLifetime
+from .days_since_tip import DaysSinceTip, RelDaysSinceTip
 from .dynamic_feature_test_1 import DynamicFeatureTest1
 from .dynamic_feature_test_2 import DynamicFeatureTest2
+from .mean_ordered_rate import MeanOrderedRate
 from .mode_department import ModeDepartment
+from .order_frequency import OrderFrequency
 from .order_size import OrderSize
+from .prev_order_tipped import PrevOrderTipped
 from .prev_tipped_products_ratio import PrevTippedProductsRatio
 from .reordered_ratio import ReorderedRatio
 from .tip_history import TipHistory
 from .tip_rate import ProductTipRate, DepartmentTipRate, AisleTipRate
-from .order_frequency import OrderFrequency
-from .days_since_tip import DaysSinceTip, RelDaysSinceTip
-from .mean_ordered_rate import MeanOrderedRate
