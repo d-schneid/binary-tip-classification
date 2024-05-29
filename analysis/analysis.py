15d8a48599e268532a93a2cc7c04cb4fef8ab302
I538883 <kilian.kempf@sap.com>
2024-05-30 00:16:22 +0200

    small fix

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -2,11 +2,12 @@ from abc import ABC, abstractmethod
 
 
 class Analysis(ABC):
-    
+
     def __init__(self, data_manager):
         self.orders_tip = data_manager.get_orders_tip(complete=True)
         self.orders_tip = self.orders_tip[self.orders_tip['eval_set'] == 'prior']
-        self.orders_joined = data_manager.get_orders_joined()
+        self.orders_joined = data_manager.get_orders_joined(complete=True)
+        self.orders_joined = self.orders_joined[self.orders_joined['eval_set'] == 'prior']
         self.products = data_manager.get_products()
         self.departments = data_manager.get_departments()
         self.aisles = data_manager.get_aisles()
