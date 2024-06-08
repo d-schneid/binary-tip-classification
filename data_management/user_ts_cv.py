5ab0b9eb22541bbabd5ee9a66c162d8362862d6f
I538883 <kilian.kempf@sap.com>
2024-06-08 18:37:38 +0200

    fix splitter implementation

--- data_management/user_ts_cv.py
+++ data_management/user_ts_cv.py
@@ -5,11 +5,12 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class UserTSCVSplitter(BaseEstimator, TransformerMixin):
 
     def __init__(self, data_manager, n_splits, validation_set_ratio):
+        self.data_manager = data_manager
         self.n_splits = n_splits
         self.validation_set_ratio = validation_set_ratio
-        self.validation_sets = self._assign_cv_validation_set(data_manager)
+        self.validation_sets = self._assign_cv_validation_set()
 
-    def _assign_cv_validation_set(self, data_manager):
+    def _assign_cv_validation_set(self):
         """
         Assigns each point of the given user time-series to its respective validation set for cross-validation.
         It is iterated across the given user time-series and the last 'validation_set_ratio'% points of the remaining
@@ -19,7 +20,7 @@ class UserTSCVSplitter(BaseEstimator, TransformerMixin):
         :return: The given user time-series along with a label 'cv_validation_set' indicating to which validation set
                  the respective point belongs to for cross-validation.
         """
-        validation_sets = data_manager.get_orders_tip()[['user_id', 'order_number']].copy()
+        validation_sets = self.data_manager.get_orders_tip_train()[['user_id', 'order_number']].copy()
         validation_sets = validation_sets.groupby('user_id').apply(
             self._assign_cv_validation_set_by_user,
             include_groups=False).reset_index(drop=True)
@@ -57,6 +58,8 @@ class UserTSCVSplitter(BaseEstimator, TransformerMixin):
         for i in range(1, self.n_splits + 1):
             train = X.index[self.validation_sets['cv_validation_set'] > i].tolist()
             test = X.index[self.validation_sets['cv_validation_set'] == i].tolist()
+
+            print(f'Iteration {i}: Train size: {len(train)}, Test size: {len(test)}')
             yield train, test
 
     def get_n_splits(self, X, y, groups=None):
